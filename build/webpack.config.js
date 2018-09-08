27b50668fc1623ffea4afae70e5e62140b6f48fd
hongweiyu <yuxinburendavie@gmail.com>
2018-09-08 22:49:33 +0800

    build first

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,9 +1,3 @@
-/**
- * 
- * @authors Your Name (you@example.org)
- * @date    2017-08-31 11:40:50
- * @version $Id$
- */
 const path = require('path')
 
 module.exports = {
@@ -11,14 +5,16 @@ module.exports = {
 		app: path.join(__dirname, '../client/app.js')
 	},
 	output: {
-		filename: '[name].[hash].js',
+		filename: '[name][hash].js',
 		path: path.join(__dirname, '../dist'),
 		publicPath: '/public'
 	},
 	module: {
-		rules: [{
-			test: /.jsx$/,
-			loader: 'babel-loader'
-		}]
+		rules: [
+			{
+				test: /.jsx$/,
+				loader: 'babel-loader'
+			}
+		]
 	}
 }
--- client/app.js
+++ client/app.js
@@ -1,11 +1,4 @@
-/**
- * 
- * @authors xiaolei (you@example.org)
- * @date    2017-08-31 11:39:33
- * @version $Id$
- */
-
 import ReactDOM from 'react-dom'
-import APP from './App.jsx'
+import App from './App.jsx'
 
-ReactDOM.render(App, document.body)
+ReactDOM.render(App, documment.body)
